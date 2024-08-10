dfe3c31da78f16745235bad8fe3fe518fb3d31d4
acho <opehn0@gmail.com>
2024-08-10 09:10:02 +0900

    [fix] Teather와 TicketSeller의 결합도 낮추기
    - TicketSeller가 하는 일을 Theather가 알지 못하게 모두 TicketSeller 안으로 숨겼다.
    - 결합도를 낮추어 Theather 객체의 의존도가 낮아지고, TicketSeller 객체의 자율성이 높아졌다.

--- lecture1-wrongExample/src/Theater.java
+++ lecture1-wrongExample/src/Theater.java
@@ -6,22 +6,6 @@ public class Theater {
     }
 
     public void enter(Audience audience) {
-        Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-
-        if (audience.getBag().hasInvitation()) {
-            audience.getBag().setTicket(ticket);
-            return;
-        }
-
-        final Long audienceAmount = audience.getBag().getAmount();
-
-        if (audienceAmount < ticket.getFee()) {
-            ticketSeller.getTicketOffice().addTicket(ticket);
-            return;
-        } else {
-            audience.getBag().minusAmount(ticket.getFee());
-            ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
-            audience.getBag().setTicket(ticket);
-        }
+        this.ticketSeller.sellTo(audience);
     }
 }
--- lecture1-wrongExample/src/TicketSeller.java
+++ lecture1-wrongExample/src/TicketSeller.java
@@ -5,7 +5,23 @@ public class TicketSeller {
         this.ticketOffice = ticketOffice;
     }
 
-    public TicketOffice getTicketOffice() {
-        return ticketOffice;
+    public void sellTo(Audience audience) {
+        Ticket ticket = ticketOffice.getTicket();
+
+        if (audience.getBag().hasInvitation()) {
+            audience.getBag().setTicket(ticket);
+            return;
+        }
+
+        final Long audienceAmount = audience.getBag().getAmount();
+
+        if (audienceAmount < ticket.getFee()) {
+            ticketOffice.addTicket(ticket);
+            return;
+        } else {
+            audience.getBag().minusAmount(ticket.getFee());
+            ticketOffice.plusAmount(ticket.getFee());
+            audience.getBag().setTicket(ticket);
+        }
     }
 }
